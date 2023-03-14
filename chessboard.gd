738246f9140c41885a4f849055737218e5de758e
Tyler K <tyly04@gmail.com>
2023-03-13 18:01:48 -0700

    Update chessboard.gd

--- chessboard.gd
+++ chessboard.gd
@@ -1,21 +1,34 @@
 extends Node
 
+class GameState:
+	# Play means continue as normal, Draw means both sides have lost, Check means a side is in danger of losing, Checkmate means a side has lost.
+	enum Type {PLAY, DRAW, CHECK, CHECKMATE};
+	var type : Chessboard.GameState.Type;
+	# null if there's currently no piece in check.
+	# If the type is CHECKMATE, then this side has lost:
+	var inCheck : Chessboard.Piece;
+
+
+# Given by get_possible_moves:
 class Move:
-	enum Type {MOVE, CAPTURE, CASTLE, PROMOTION}
+	enum Type {MOVE, CAPTURE, CASTLE}
 	var type : Chessboard.Move.Type;
 	var position : Vector2;
-	func execute():
-		pass
+	# Perform the actual move and update Chessboard. Will also return GameState to tell you important information about the game (has a side won? Lost? Is there a draw?)
+	func execute() -> Chessboard.GameState:
+		return GameState.new();
 
 class Piece:
 	enum Side {WHITE, BLACK}
 	enum Type {PAWN, ROOK, KNIGHT, BISHOP, QUEEN, KING}
-	var type;
-	var side;
-	static func new_piece(_type : Chessboard.Piece.Type = Type.PAWN, _side: Chessboard.Piece.Side = Side.WHITE):
+	var type : Chessboard.Piece.Type;
+	var side : Chessboard.Piece.Side;
+	var position : Vector2;
+	static func new_piece(_type : Chessboard.Piece.Type = Type.PAWN, _side: Chessboard.Piece.Side = Side.WHITE, _pos: Vector2 = Vector2.ZERO):
 		var p = Piece.new();
 		p.type = _type;
 		p.side = _side;
+		p.position = _pos;
 		return p;
 	func get_possible_moves() -> Array[Chessboard.Move]:
 		return [];
@@ -58,8 +71,8 @@ func _ready():
 func ClearBoard():
 	var layout = [Piece.Type.ROOK, Piece.Type.KNIGHT, Piece.Type.BISHOP, Piece.Type.QUEEN, Piece.Type.KING, Piece.Type.BISHOP, Piece.Type.KNIGHT, Piece.Type.ROOK];
 	for i in range(8):
-		SetPiece(Vector2(i, 1), Piece.new_piece(Piece.Type.PAWN, Piece.Side.WHITE));
-		SetPiece(Vector2(i, 6), Piece.new_piece(Piece.Type.PAWN, Piece.Side.BLACK));
+		SetPiece(Vector2(i, 1), Piece.new_piece(Piece.Type.PAWN, Piece.Side.WHITE, Vector2(i, 1)));
+		SetPiece(Vector2(i, 6), Piece.new_piece(Piece.Type.PAWN, Piece.Side.BLACK, Vector2(i, 6)));
 		
-		SetPiece(Vector2(i, 0), Piece.new_piece(layout[i], Piece.Side.WHITE));
-		SetPiece(Vector2(i, 7), Piece.new_piece(layout[i], Piece.Side.BLACK));
+		SetPiece(Vector2(i, 0), Piece.new_piece(layout[i], Piece.Side.WHITE, Vector2(i, 0)));
+		SetPiece(Vector2(i, 7), Piece.new_piece(layout[i], Piece.Side.BLACK, Vector2(i, 7)));
